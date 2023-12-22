9bb4c3650e7762c97a5eb140406f1fa0aea4a463
matan <matan@iammaran.com>
2023-12-22 12:48:37 +0200

    turns out you can just type WEB_DIRECTORY and it will add it wow

--- __init__.py
+++ __init__.py
@@ -5,9 +5,7 @@ import __main__
 
 import requests
 
-DEV_MODE = True
-extensions_folder = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
-                                 "web" + os.sep + "extensions" + os.sep + "ComfyUI-Assistant")
+WEB_DIRECTORY = 'javascript'
 javascript_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "javascript")
 
 # Get latest data
@@ -18,23 +16,3 @@ if os.getenv("COMFY_ASSISTANT_ENV") != "DEV":
     if response.status_code == 200:
         with open(data_js_path, "w", encoding="utf-8") as local_file:
             local_file.write(response.text)
-
-
-
-
-
-if not os.path.exists(extensions_folder):
-    os.makedirs(extensions_folder)
-
-result = filecmp.dircmp(javascript_folder, extensions_folder)
-
-if result.left_only or result.diff_files:
-    file_list = list(result.left_only)
-    file_list.extend(x for x in result.diff_files if x not in file_list)
-
-    for file in file_list:
-        src_file = os.path.join(javascript_folder, file)
-        dst_file = os.path.join(extensions_folder, file)
-        if os.path.exists(dst_file):
-            os.remove(dst_file)
-        shutil.copy(src_file, dst_file)
